5cd83ae475426b25206a6af1993e546fa2392886
inderkiranbhamra <140848814+inderkiranbhamra@users.noreply.github.com>
2025-04-16 18:20:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Navbar from './components/NavBar'
 import HeroSection from './components/HeroSection'
 import CarouselSection from './components/CarouselSection'
@@ -12,20 +10,20 @@ import ClientsSection from './components/ClientsSection'
 import ContactUs from './components/ContactUs'
 
 function App() {
-
   return (
     <>
       <Navbar />
-      <div className="pt-20">  
-      <section id="home"><HeroSection /></section>
-      <section id="about"><AboutUs /></section>
-      <section id="services"><CarouselSection /></section>
-      <section ><HowItWorks /></section>
-{/*       <section id="clients"><TestimonialCarousel /></section> */}
-      <section ><ClientsSection /></section>
-      <section id="contact">
-      <ContactUs />
-      <Footer /></section>
+      <div className="pt-20">
+        <section id="home"><HeroSection /></section>
+        <section id="about"><AboutUs /></section>
+        <section id="services"><CarouselSection /></section>
+        <section id="how-it-works"><HowItWorks /></section>
+        {/* <section id="clients"><TestimonialCarousel /></section> */}
+        <section id="clients"><ClientsSection /></section>
+        <section id="contact">
+          <ContactUs />
+          <Footer />
+        </section>
       </div>
     </>
   )
